623c875436bfd72dd351ceeffc76f1b5f6e4cfd3
pc <pc@DESKTOP-K1MJT8V>
2022-03-14 10:18:55 +0900

    버전 : 2.0 변경내용 : 로그인 기능 업그레이드

--- test826java/src/test826java/test.java
+++ test826java/src/test826java/test.java
@@ -3,7 +3,9 @@ package test826java;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("version : 2.0");
+		System.out.println("로그인 됨!");
+		
 	}
 
 }

0baabe7017b8628b600a706f71cb0afe664a37a6
pc <pc@DESKTOP-K1MJT8V>
2022-03-14 10:27:04 +0900

    버전 : 4.5

--- test826java/src/test826java/test.java
+++ test826java/src/test826java/test.java
@@ -3,7 +3,7 @@ package test826java;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("version : 3.0");
+		System.out.println("version : 4.0");
 		System.out.println("로그인 됨!");
 		
 	}
